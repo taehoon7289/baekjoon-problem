ba1d9aa31a7718bc52e4f630d217bfbea23057a3
taehoon7289 <taehoon7289@gmail.com>
2022-08-25 13:55:20 +0900

    feat: 25304 해결
    - 25304 해결

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,7 @@ fun main(args: Array<String>) {
 //    val problem10430 = Problem_10430()
 //    problem10430.execute()
 
-    val problem2480 = Problem_2480()
-    problem2480.execute2()
+    val problem25304 = Problem_25304()
+    problem25304.execute()
 
 }
